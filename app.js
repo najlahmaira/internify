5f85c7a182f211ed662a8af5ad5e475c3869698f
najlahmaira <112544930+najlahmaira@users.noreply.github.com>
2024-06-09 16:58:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = require('./routes/index')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
